2e1f65d0253cad319de5a34634cd545276c3e35f
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-24 00:09:00 +0600

    Dynamic Object With Constructor

---
+++ 4_dynamic_object_with_constructor.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    char name[101];
+    int roll;
+    int cls;
+    char section;
+    Student(char *n,int r,int c,int s)
+    {
+        strcpy(name,n);
+        roll = r;
+        cls = c;
+        section = s;
+    }
+};
+
+int main()
+{
+    char nm[101] = "Asif Abdullah";
+    Student* asif = new Student(nm,18,8,'B');
+
+    
+    cout << "Asif's Information:" << endl;
+    cout << asif->name << endl;
+    cout << asif->roll << endl;
+    cout << asif->section << endl;
+    cout << asif->cls << endl;
+
+    return 0;
+}
