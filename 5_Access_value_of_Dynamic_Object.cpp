a96ba20ce3cff8be163cb574545527b9a83f512e
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-24 01:04:55 +0600

    Accessing the value of Dynamic Object

--- 5_Access_value_of_Dynamic_Object.cpp
+++ 5_Access_value_of_Dynamic_Object.cpp
@@ -22,6 +22,8 @@ int main()
     char nm[101] = "Asif Abdullah";
     Student* asif = new Student(nm,18,8,'B'); //dynamic object creation
 
+    (*asif).roll = 10; // jodi kono kisu update korte chai taholeo direfference korte hoy (dynamic object er khetre) ///
+
     cout << "Asif's Information:" << endl;
     cout << (*asif).name << endl; //first dereference asif then access the name of asif
     cout << (*asif).roll << endl;  // normal int er khetre jemon pointer ke direfference kore value access korte hoy, oi ek e system eta //
@@ -29,6 +31,7 @@ int main()
     cout << (*asif).cls << endl << endl; //(*asif).cls evabe bracket na dile compiler *(asif.cls) dhore nibe tai thik moto kaj krbe na
 
     //Shortcut:
+     asif->roll = 9;
 
     //arrow sign: (*asif).name = asif->name
 
